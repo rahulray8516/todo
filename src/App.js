49c5321cf7bbf586fb5e3bb596e297648bbeb904
RAHUL RAY <ray.rahul.8101@gmail.com>
2023-08-14 11:48:56 +0530

    s

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Hi Guys</code> and save to reload.
+          Edit <code>Hi Guys, how are you</code> and save to reload.
         </p>
         <a
           className="App-link"
